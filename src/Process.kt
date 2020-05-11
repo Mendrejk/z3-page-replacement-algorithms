f06460776554faa547d7959d32c1e74a2111e1e4
Mendrejk <bastian.lakomy@gmail.com>
2020-05-11 23:44:47 +0200

    Added generation of disk page references for a process. Includes locality of reference.

---
+++ src/Process.kt
@@ -0,0 +1,50 @@
+class Process(pageCount: Int) {
+    val pages: List<Int> = (0 until pageCount).toList()
+    private val localityBaseChance: Int = LOCALITY_BASE_CHANCE
+    private val localityGain: Int = LOCALITY_GAIN
+    var localityCurrentChance = localityBaseChance
+        private set
+    var isLocality: Boolean = false
+        private set
+    private var localityReferencesLeft: Int = 0
+    var localitycurrentRange: IntRange = IntRange(0, 0)
+        private set
+    private val localityMinimalScope = LOCALITY_MINIMAL_SCOPE
+    private val localityMaximalScope = LOCALITY_MAXIMAL_SCOPE
+    private val localityMinimalLength = LOCALITY_MINIMAL_LENGTH
+    private val localityMaximalLength = LOCALITY_MAXIMAL_LENTH
+
+    fun incrementLocalityChance(): Unit {
+        localityCurrentChance += localityGain
+        if (localityCurrentChance > 100) localityCurrentChance = 100
+    }
+
+    fun startLocality() {
+        isLocality = true
+        generateLocalityRange()
+        generateLocalityLength()
+    }
+
+    fun stopLocality() {
+        isLocality = false
+        localityCurrentChance = localityBaseChance
+    }
+
+    fun tickLocality(): Unit {
+        localityReferencesLeft--;
+    }
+
+    fun isLocalityReferencesLeftZero(): Boolean = localityReferencesLeft == 0
+
+    private fun generateLocalityRange(): Unit {
+        val rangeLength = generateLocalityScope()
+        val rangeStartIndex = (0 until (pages.size - rangeLength)).random()
+        localitycurrentRange = pages[rangeStartIndex]..pages[rangeStartIndex + rangeLength]
+    }
+
+    private fun generateLocalityLength(): Unit {
+        localityReferencesLeft = (localityMinimalLength..localityMaximalLength).random()
+    }
+
+    private fun generateLocalityScope(): Int = (localityMinimalScope..localityMaximalScope).random()
+}
---
+++ src/ReferenceGenerator.kt
@@ -0,0 +1,15 @@
+fun generateReferences(howMany: Int, process: Process, useLocality: Boolean): List<Int> = List(howMany) {
+    // leaving this at-first-glance useless asignment to ease future convertion into a list of processes
+    val itProcess = process
+    if (useLocality) {
+        if (itProcess.isLocality) {
+            itProcess.tickLocality()
+            if (itProcess.isLocalityReferencesLeftZero()) itProcess.stopLocality()
+            itProcess.localitycurrentRange.random()
+        } else {
+            if ((0..100).random() <= process.localityCurrentChance) process.startLocality()
+            process.incrementLocalityChance()
+            process.pages.random()
+        }
+    } else process.pages.random()
+}
